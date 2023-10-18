28c2a5552e3e158ca89128bd868d160d4fe3c69b
olpol <omwfoster@gmail.com>
2023-10-18 11:34:37 +0100

    build with microros custom transport

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,11 @@
 #include "custom.h"
 #include "stepper/stepper_C.h"
 #include "beam_breaker/beam_breaker.h"
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <rmw_microros/custom_transport.h>
 
 DMA2D_HandleTypeDef hdma2d;
 DSI_HandleTypeDef hdsi;
@@ -35,6 +40,7 @@ void StartDefaultTask(void const *argument);
 void ui_thread(void const *arg);
 void LVGLTimer(void const *arg);
 void LGVLTick(void const *arg);
+void uros_thread(void const *arg);
 
 static void SystemClock_Config(void);
 static void CPU_CACHE_Enable(void);
@@ -48,6 +54,15 @@ extern "C" void TIM11_IRQHandler(void);
 
 rcl_publisher_t publisher;
 std_msgs__msg__Int32 msg;
+bool cubemx_transport_open(struct uxrCustomTransport * transport);
+bool cubemx_transport_close(struct uxrCustomTransport * transport);
+size_t cubemx_transport_write(struct uxrCustomTransport* transport, const uint8_t * buf, size_t len, uint8_t * err);
+size_t cubemx_transport_read(struct uxrCustomTransport* transport, uint8_t* buf, size_t len, int timeout, uint8_t* err);
+
+void * microros_allocate(size_t size, void * state);
+void microros_deallocate(void * pointer, void * state);
+void * microros_reallocate(void * pointer, size_t size, void * state);
+void * microros_zero_allocate(size_t number_of_elements, size_t size_of_element, void * state);
 
 void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
 {
@@ -209,6 +224,9 @@ int main(void)
   lvgl_timerHandle = osThreadCreate(osThread(LVGLTimer), NULL);
 
   osThreadDef(ui_thread, ui_thread, osPriorityNormal, 0, 4096);
+  defaultTaskHandle = osThreadCreate(osThread(ui_thread), NULL);
+
+  osThreadDef(uros_thread, uros_thread, osPriorityNormal, 0, 4096);
   defaultTaskHandle = osThreadCreate(osThread(ui_thread), NULL);
 
   /* Start scheduler */
@@ -585,12 +603,78 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 
 void ui_thread(void const *arg)
 {
-
   while (1)
   {
     lv_task_handler();
     osDelay(5);
   }
+}
+
+void uros_thread(void const * arg)
+{
+
+    /* USER CODE BEGIN StartDefaultTask */
+    /* Infinite loop */
+    // micro-ROS configuration
+
+/*     rmw_uros_set_custom_transport(
+      true,
+      (void *) &huart2,
+      cubemx_transport_open,
+      cubemx_transport_close,
+      cubemx_transport_write,
+      cubemx_transport_read); */
+
+    rcl_allocator_t freeRTOS_allocator = rcutils_get_zero_initialized_allocator();
+    freeRTOS_allocator.allocate = microros_allocate;
+    freeRTOS_allocator.deallocate = microros_deallocate;
+    freeRTOS_allocator.reallocate = microros_reallocate;
+    freeRTOS_allocator.zero_allocate =  microros_zero_allocate;
+
+    if (!rcutils_set_default_allocator(&freeRTOS_allocator)) {
+        printf("Error on default allocators (line %d)\n", __LINE__);
+    }
+
+    // micro-ROS app
+
+    rcl_publisher_t publisher;
+    std_msgs__msg__Int32 msg;
+    rclc_support_t support;
+    rcl_allocator_t allocator;
+    rcl_node_t node;
+
+    allocator = rcl_get_default_allocator();
+
+    //create init_options
+    rclc_support_init(&support, 0, NULL, &allocator);
+
+    // create node
+    rclc_node_init_default(&node, "cubemx_node", "", &support);
+
+    // create publisher
+    rclc_publisher_init_default(
+      &publisher,
+      &node,
+      ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+      "cubemx_publisher");
+
+    msg.data = 0;
+
+    for(;;)
+    {
+      rcl_ret_t ret = rcl_publish(&publisher, &msg, NULL);
+      if (ret != RCL_RET_OK)
+      {
+        printf("Error publishing (line %d)\n", __LINE__);
+      }
+
+      msg.data++;
+      osDelay(10);
+    
+    /* USER CODE END StartDefaultTask */
+}
+
+
 }
 /**
  * @brief  This function is executed in case of error occurrence.
